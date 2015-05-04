2d590e544615fe130b5c80362dab3fb65da09b6d
Poshan <poshan.niraula@gmail.com>
2015-05-04 18:14:04 +0545

    cluster not working

--- map.js
+++ map.js
@@ -1,32 +1,43 @@
 function init (){
-  var map = L.map('map').setView([27.714, 85.319], 13);
+  //console.log('inside the init');
+  var north_east = new L.latLng(26.328231, 80.029907);
+  var south_west = new L.latLng(30.605155, 88.225708);
+  var bounds = new L.latLngBounds(north_east, south_west);
+   map = L.map('map').setView([27.6933, 85.5643], 9);
+  
     osmUrl = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
     mapbox = 'https://{switch:a,b,c,d}.tiles.mapbox.com/v4/mapbox.satellite-kathmandu-20150425-after/{zoom:7}/{x}/{y}.jpg?access_token=pk.eyJ1Ijoib3BlbnN0cmVldG1hcCIsImEiOiJhNVlHd29ZIn0.ti6wATGDWOmCnCYen-Ip7Q';
     osmAttribution = 'Map Tiles &copy; CC BY-SA <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
-
+   //map.fitBounds(bounds);
 /*for osm layer*/
     var osm = L.tileLayer(osmUrl, {
         attribution: osmAttribution,
         doubleClickZoom: true
       }).addTo(map);
 
+/*
+TO-dos
+1. Add post disaster imageries
+2. Add other data (new data)
+3. cluster the markers
+4. add description and attribution
+*/
 
-    var mapbox = L.tileLayer(mapbox);
 
-    var vilname= L.tileLayer.wms('http://localhost:8080/geoserver/NepalEarthquake/wms',{
-    layers: 'villagename',
+    var vdc= L.tileLayer.wms('http://202.45.144.203:8080/geoserver/earthquake/wms',{
+    layers: 'VDC',
     zoom:7.5,
-    opacity: .2,
+    opacity: .4,
     transparent: true
 
   });
-var vdc=L.tileLayer.wms('http://localhost:8080/geoserver/NepalEarthquake/wms',{
-  layers:'vdc',
+var vilname=L.tileLayer.wms('http://202.45.144.203:8080/geoserver/earthquake/wms',{
+  layers:'Village_name',
   transparent: true,
-  opacity: .2,
-    });
+  opacity: .4,
+});
 
-var trans=L.tileLayer.wms('http://localhost:8080/geoserver/NepalEarthquake/wms',{
+/*var trans=L.tileLayer.wms('http://localhost:8080/geoserver/NepalEarthquake/wms',{
   layers:'trans_ln',
   transparent: true,
   opacity: .5,
@@ -36,22 +47,18 @@ var hydro=L.tileLayer.wms('http://localhost:8080/geoserver/NepalEarthquake/wms',
   layers:'hydro_ln',
   transparent: true,
   opacity: .5,
-    });
+    });*/
 
 
-
-
-    
-  // var pruneCluster = new PruneClusterForLeaflet();
-  // var marker = new PruneCluster.Marker(latitude, longitude);
-  // pruneCluster.RegisterMarker(marker);
-  // leafletMap.addLayer(pruneCluster);
+  damaged_buildings_cluster = L.markerClusterGroup();
 
   function popup_show(f,l){
-      // console.log(f.properties.Description);
+      // console.log(f.geometry.coordinates);
       // debugger;
       // var marker = new PruneCluster.Marker(f.geometry.coordinates);
-      // pruneCluster.RegisterMarker(marker);
+      marker = new L.marker(f.geometry.coordinates);
+      damaged_buildings_cluster.addLayer(marker);
+      
       var popUpContent = f.properties.Description;
       l.bindPopup(L.popup({
           closeOnClick: true,
@@ -63,9 +70,11 @@ var hydro=L.tileLayer.wms('http://localhost:8080/geoserver/NepalEarthquake/wms',
       }).setContent(popUpContent));
   }
 
-  var damaged_buildings = new L.geoJson.ajax('damaged_buildings.geojson',{
+var damaged_buildings = new L.geoJson.ajax('damaged_buildings.geojson',{
     onEachFeature : popup_show
   });
+
+
   var road_damages = new L.geoJson.ajax('damaged_buildings.geojson',{
     onEachFeature : popup_show
   });
@@ -74,22 +83,22 @@ var hydro=L.tileLayer.wms('http://localhost:8080/geoserver/NepalEarthquake/wms',
   });
   // map.addLayer(pruneCluster);
   var baseLayers = {
-    "Post-Disaster Image":mapbox,
+    //"Post-Disaster Image":mapbox,
     "OpenStreetMap": osm
     // "SD": topodata      
   }
+   damaged_buildings_cluster.addLayer(damaged_buildings);
   var overlays = {
-    "Damaged Buildings" : damaged_buildings,
+    "Damaged Buildings" : damaged_buildings_cluster,
     "Damaged Road" : road_damages,
     "Major Destructions" : major_destructions,
     "Village Name":vilname,
     "VDC":vdc,
-    "Transportation":trans,
-    "Rivers":hydro
-    }
- 
-  
+    //"Transportation":trans,
+    //"Rivers":hydro
+  }
+  map.addLayer(damaged_buildings_cluster);
   L.control.layers(baseLayers,overlays).addTo(map);
-
+  
 
 }
